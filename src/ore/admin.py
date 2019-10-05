c2100530eb75ed6823693220a6500b861a0bcd6a
Mario <56191794+djangopythonmario@users.noreply.github.com>
2019-10-05 15:18:45 +0200

    Update admin.py

--- src/ore/admin.py
+++ src/ore/admin.py
@@ -1,71 +1,7 @@
-from django import forms
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django.contrib import admin
+
+# Register your models here.
 from .models import Ore 
 
+admin.site.register(Ore) 
 
-class CreateNewOreupdate(forms.ModelForm):
-      class Meta:  
-            model = Ore
-            fields = ('oret','contrattiok','contrattiko','nomecognome','statuse',)
-            widgets = {
-                 'data' : forms.DateInput(
-                attrs={
-               'class': 'md-form'
-                    }
-                ),
-                'user' : forms.Select(
-                attrs={
-                'class': 'custom-select',
-                    }
-                ),
-                'contrattiok' : forms.NumberInput(
-                attrs={
-                'class': 'form-control',
-                    }
-                ),               
-                'oret' : forms.NumberInput(
-                attrs={
-                'class': 'form-control'
-                    }
-                ),
-                'nomecognome' : forms.TextInput(
-                attrs={
-                'class': 'form-control'
-                    }
-                ),   
-                }  
-
-class CreateNewOre(forms.ModelForm):
-      class Meta:  
-            model = Ore 
-            exclude = ('statuse','contrattiko', ) 
-            fields = ('user','oret','contrattiok','contrattiko','nomecognome','statuse',)
-            widgets = {
-                 'data' : forms.DateInput(
-                attrs={
-               'class': 'md-form'
-                    }
-                ),
-                'user' : forms.Select(
-                attrs={
-                'class': 'custom-select',
-                    }
-                ),
-                'contrattiok' : forms.NumberInput(
-                attrs={
-                'class': 'form-control'
-                    }
-                ),               
-                'oret' : forms.NumberInput(
-                attrs={
-                'class': 'form-control'
-                    }
-                ),
-                'nomecognome' : forms.TextInput(
-                attrs={
-                'class': 'form-control'
-                    }
-                ),  
-                }   
